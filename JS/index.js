f570136441ae23193fa7258e756655207ea079b4
AlexxGen <alexx.genchev@gmail.com>
2024-07-04 10:02:51 -0600

    fixed the name formatting in the css files

--- JS/index.js
+++ JS/index.js
@@ -3,8 +3,12 @@ var clickDiv = document.querySelectorAll(".clickable");
 for(var i=0; i < clickDiv.length; i++){
     clickDiv[i].addEventListener("mouseover", function(){
         this.classList.add("hover");
+        this.querySelector("a").querySelector(".name").classList.add("hoverName");
+        this.querySelector("a").querySelector(".photo").classList.add("hoverImg");
     })
     clickDiv[i].addEventListener("mouseout", function(){
         this.classList.remove("hover");
+        this.querySelector("a").querySelector(".name").classList.remove("hoverName");
+        this.querySelector("a").querySelector(".photo").classList.remove("hoverImg");
     })
 }
